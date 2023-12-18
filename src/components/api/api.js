7d6142e681d0c8f36243997fa868b4521ce117bc
johnmarcomanalo <jmamanalo@gzonetechph.com>
2023-12-18 11:27:04 +0800

    update api
    api: http://122.54.107.118/backend/api.workflow/

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -3,12 +3,14 @@ import axios from "axios";
 // let serverurl = "http://api.pacificweb.com.ph/";
 // let serverurl = "http://localhost/api";
 // let serverurl = "https://api.workflow.gzonetechph.com/";
-let serverurl = "https://workflowapi.greenzoneph.com/";
+// let serverurl = "https://workflowapi.greenzoneph.com/";
+let serverurl = "http://122.54.107.118/backend/api.workflow/";
 // let serverurl ="http://192.168.0.9/backend/api/";
 // let serverurl ="http://192.168.5.21/backend/beta_api/";
 // let serverurl = "http://beta.api.pacificweb.com.ph/";
 // let serverurl ="http://192.168.1.44/api_pacific/";
 // let serverurl ="http://192.168.2.36/beta_api3/";
+
 let key = "?key=PocketHR@20190208&type=web";
 let key2 = "?key=12345ABCFleet";
 const CancelToken = axios.CancelToken;
